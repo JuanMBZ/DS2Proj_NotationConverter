b742e90bdc6e40b4bad07c910e1ad6f706f4b823
JoshuaLatagan <joshuaregalalatagan@gmail.com>
2025-05-01 19:21:12 +0800

    Update expression_tree.h

--- expression_tree.h
+++ expression_tree.h
@@ -11,6 +11,7 @@
 
 expr_tree *infix_to_exprtree(char *input);
 void infix_traversal(expr_tree *root);
-expr_tree *prefix_to_exprtree(char *input);  // New declaration
+expr_tree *prefix_to_exprtree(char *input);  
+void prefix_traversal(expr_tree *root);
 
 #endif
